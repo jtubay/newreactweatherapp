69e35cdf0215d5f1bf193bd92b26daabb743b711
Jean Tubay <jean.tubay.92@gmail.com>
2020-02-05 21:53:22 -0500

    Updates

---
+++ precache-manifest.3ac95c01e2bab8c42b3af91f16f197a7.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d31d4990b178f17517b113261ea95c2",
+    "url": "/newreactweatherapp/index.html"
+  },
+  {
+    "revision": "ea1de3cf8be9d644836a",
+    "url": "/newreactweatherapp/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "1dc93ed22a2f09fcdb35",
+    "url": "/newreactweatherapp/static/css/main.b24ae078.chunk.css"
+  },
+  {
+    "revision": "ea1de3cf8be9d644836a",
+    "url": "/newreactweatherapp/static/js/2.01088bda.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/newreactweatherapp/static/js/2.01088bda.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1dc93ed22a2f09fcdb35",
+    "url": "/newreactweatherapp/static/js/main.1b2ef18c.chunk.js"
+  },
+  {
+    "revision": "0728ff3bb8127a42b58a",
+    "url": "/newreactweatherapp/static/js/runtime-main.8100d552.js"
+  },
+  {
+    "revision": "f692efdc10686fc298c8620ec3c47c41",
+    "url": "/newreactweatherapp/static/media/17494.f692efdc.jpg"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/newreactweatherapp/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/newreactweatherapp/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/newreactweatherapp/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/newreactweatherapp/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ecaf8b481729b18f6a8494d9f691cdae",
+    "url": "/newreactweatherapp/static/media/weathericons-regular-webfont.ecaf8b48.svg"
+  }
+]);
